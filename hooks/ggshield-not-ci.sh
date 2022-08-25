146bbea85fd054fbcfd51ff2fa44e83e57b5ec93
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-25 17:56:12 +0100

    fix(ggshield-not-ci): correct logic and print when skipping

--- hooks/ggshield-not-ci.sh
+++ hooks/ggshield-not-ci.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [[ ! -z "${CI}"} ]]; then
+if [[ -z "${CI}" ]]; then
   ggshield secret scan pre-commit
+else
+  echo "In a CI environment, so skipping GitGuardian ggshield"
 fi
